883c4a2a908f542f7e6f4450ebbdbd7d0a4f6832
Sampey Hoffman <53267153+Samplehoff@users.noreply.github.com>
2020-09-02 18:04:45 -0500

    completed game

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include <ctime>
 
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
-    std::cout << "You are Starkiller rebel freedom fighter!\n";
-    std::cout << "Help the Rebels and survive the Base!\n";
+    std::cout << "\n\nYou are Starkiller breakthrough level " << Difficulty;
+    std::cout << " of the Base!\n Help the Rebels and survive the Base!\n";
 }
 
 
-void PlayGame()
+bool PlayGame(int Difficulty)
 {
 
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
 
     // Declare 3 number code
-    const int CodeA = 4;
-    const int CodeB = 3;
-    const int CodeC = 2;
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
@@ -37,17 +38,38 @@ void PlayGame()
     // Check if player is correct
     if(GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\n + You have passed the first door!";
+        std::cout << "\n ** You have passed on to the next level! **";
+        return true;
     }
     else
     {
-        std::cout << "\n + You have FAILED the Rebels!";
+        std::cout << "\n ** You have FAILED the Rebels! Retry **";
+        return false;
     };
 }
 
 
 int main()
 {
-    PlayGame();
+
+    srand(time(NULL)); //Create new random sequence based on time of day
+
+    int LevelDifficulty = 1;
+    int const MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) // Loop game until all levels completed
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();// Clears any errors.
+        std::cin.ignore();// Discards the buffer.
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+    }
+
+    std::cout << "\n\n*** Congratulations Starkiller! You have helped the Rebels! Now win this war! ***";
+    
     return 0;
 }
